90a47f98420ef791aa04476ca2f45c29c39d5b8e
Sharmake-gif <sbotan1@student.gsu.edu>
2025-02-20 18:10:47 -0500

    Update ProfessorListDisplay.java

--- spring-boot/src/main/java/ProfessorListDisplay.java
+++ spring-boot/src/main/java/ProfessorListDisplay.java
@@ -1,3 +1,26 @@
-public class proffesorratingdisplay {
+//demo example
 
+public class ProfessorListDisplay extends JFrame {
+
+    private JList<String> professorList;
+
+    public ProfessorListDisplay() {
+        setTitle("Professors List");
+        setLayout(new BorderLayout());
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+        List<String> professors = new ArrayList<>();
+        professors.add("Dr. David James - Average Rating: 4.5");
+
+        professorList = new JList<>(professors.toArray(new String[0]));
+        JScrollPane scrollPane = new JScrollPane(professorList);
+
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        ProfessorListDisplay display = new ProfessorListDisplay();
+        display.setVisible(true);
+    }
 }
