1c03c94a48daa838faac059826f1d69f623c0e90
Alex Ng <119276660+arexng0899@users.noreply.github.com>
2025-03-27 19:38:52 -0400

    Added ProfessorRepository.java

---
+++ spring-boot/src/repository/ProfessorRepository.java
@@ -0,0 +1,9 @@
+package com.example.reviewapp.repository;
+
+import com.example.reviewapp.entity.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+}
