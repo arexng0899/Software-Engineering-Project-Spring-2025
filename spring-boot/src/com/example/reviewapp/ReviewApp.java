a88c12699c2624c3fd04c9fd0856164fe8d04266
Alex Ng <119276660+arexng0899@users.noreply.github.com>
2025-03-27 19:33:03 -0400

    Update ReviewApp.java

--- spring-boot/src/com/example/reviewapp/ReviewApp.java
+++ spring-boot/src/com/example/reviewapp/ReviewApp.java
@@ -1,12 +1,7 @@
 package com.example.reviewapp;
 
-import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 @SpringBootApplication
 public class ReviewApp {
@@ -14,3 +9,4 @@ public class ReviewApp {
         SpringApplication.run(ReviewApp.class, args);
     }
 }
+
