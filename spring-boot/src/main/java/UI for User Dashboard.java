343e68b7d09b72532e3c0fa03ee4af611009f429
Sharmake-gif <sbotan1@student.gsu.edu>
2025-04-21 19:19:19 -0400

    Create UI for User Dashboard.java

---
+++ spring-boot/src/main/java/UI for User Dashboard.java
@@ -0,0 +1,8 @@
+JPanel dashboard1 = new JPanel1();
+dashboardPanel.setLayout(new BoxLayout(dashboardPanel, BoxLayout.Y_Axis));
+
+dashboardPanel.add(new JLabel(" Welcome, "user: !"))
+dashboardPanel.add(new JLabel(" Email: user@gsu.edu"));
+dashboardPanel.add(new JLabel(" Favorite Courses: CSC4350"));
+dashboardPanel.add(new JLabel( " Your Reviews:"));
+dashboardPanel.add(new JTextArea("Review for Dr.James : 5 stars, Excellent teacher!"));
