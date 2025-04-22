befc07b2ed3baf85c5d2749ff7205ce6a1420f1f
Alex Ng <119276660+arexng0899@users.noreply.github.com>
2025-04-22 00:21:33 -0400

    Add StudentRepository.java

---
+++ spring-boot/src/main/java/com/example/reviewapp/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.example.reviewapp.repository;
+
+import com.example.reviewapp.entity.Student;
+import com.example.reviewapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByUser(User user);
+}
