6f572a27fb686554f07486f323d3082f74c66093
Alex Ng <119276660+arexng0899@users.noreply.github.com>
2025-04-22 00:22:12 -0400

    Update ProfessorService.java

--- spring-boot/src/main/java/com/example/reviewapp/service/ProfessorService.java
+++ spring-boot/src/main/java/com/example/reviewapp/service/ProfessorService.java
@@ -1,6 +1,7 @@
 package com.example.reviewapp.service;
 
 import com.example.reviewapp.entity.Professor;
+import com.example.reviewapp.entity.User;
 import com.example.reviewapp.repository.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,20 +18,29 @@ public class ProfessorService {
         return professorRepository.findAll();
     }
     
-    public Optional<Professor> findByName(String name) {
-        return professorRepository.findByName(name);
+    public Optional<Professor> findById(Long id) {
+        return professorRepository.findById(id);
     }
     
-    public List<Professor> searchByName(String name) {
-        return professorRepository.findByNameContainingIgnoreCase(name);
+    public Optional<Professor> findByUser(User user) {
+        return professorRepository.findByUser(user);
+    }
+    
+    public List<Professor> searchByUsername(String username) {
+        return professorRepository.findByUser_UsernameContainingIgnoreCase(username);
     }
     
     public List<Professor> searchByDepartment(String department) {
         return professorRepository.findByDepartmentContainingIgnoreCase(department);
     }
     
-    public List<Professor> searchByNameOrDepartment(String query) {
-        return professorRepository.findByNameContainingIgnoreCaseOrDepartmentContainingIgnoreCase(query, query);
+    public List<Professor> searchByUniversity(String university) {
+        return professorRepository.findByUniversityContainingIgnoreCase(university);
+    }
+    
+    public List<Professor> searchByAll(String query) {
+        return professorRepository.findByDepartmentContainingIgnoreCaseOrUniversityContainingIgnoreCaseOrUser_UsernameContainingIgnoreCase(
+                query, query, query);
     }
     
     public Professor save(Professor professor) {
