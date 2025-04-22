b5f5ae9d4ce5f1df401f79d03041e17c07fd1405
Alex Ng <119276660+arexng0899@users.noreply.github.com>
2025-04-22 00:24:13 -0400

    Add StudentService.java

---
+++ spring-boot/src/main/java/com/example/reviewapp/service/StudentService.java
@@ -0,0 +1,27 @@
+package com.example.reviewapp.service;
+
+import com.example.reviewapp.entity.Student;
+import com.example.reviewapp.entity.User;
+import com.example.reviewapp.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+    
+    public Student save(Student student) {
+        return studentRepository.save(student);
+    }
+    
+    public Optional<Student> findById(Long id) {
+        return studentRepository.findById(id);
+    }
+    
+    public Optional<Student> findByUser(User user) {
+        return studentRepository.findByUser(user);
+    }
+}
